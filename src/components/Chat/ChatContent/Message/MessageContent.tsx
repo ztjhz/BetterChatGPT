b590ddf4121b3c3bf88b8a2e2f2572a41e058f07
Holden <34213478+hodlen@users.noreply.github.com>
2023-03-07 22:56:43 +0800

    add time picker input

--- src/components/Chat/ChatContent/Message/MessageContent.tsx
+++ src/components/Chat/ChatContent/Message/MessageContent.tsx
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from 'react';
-import ReactMarkdown from 'react-markdown';
-import rehypeKatex from 'rehype-katex';
-import remarkMath from 'remark-math';
-import remarkGfm from 'remark-gfm';
-import hljs from 'highlight.js';
-import DOMPurify from 'dompurify';
-import useStore from '@store/store';
+import React, { useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
+import remarkGfm from "remark-gfm";
+import hljs from "highlight.js";
+import DOMPurify from "dompurify";
+import useStore from "@store/store";
 
-import CopyIcon from '@icon/CopyIcon';
-import EditIcon2 from '@icon/EditIcon2';
-import DeleteIcon from '@icon/DeleteIcon';
-import TickIcon from '@icon/TickIcon';
-import CrossIcon from '@icon/CrossIcon';
-import RefreshIcon from '@icon/RefreshIcon';
-import DownChevronArrow from '@icon/DownChevronArrow';
+import CopyIcon from "@icon/CopyIcon";
+import EditIcon2 from "@icon/EditIcon2";
+import DeleteIcon from "@icon/DeleteIcon";
+import TickIcon from "@icon/TickIcon";
+import CrossIcon from "@icon/CrossIcon";
+import RefreshIcon from "@icon/RefreshIcon";
+import DownChevronArrow from "@icon/DownChevronArrow";
 
-import useSubmit from '@hooks/useSubmit';
+import useSubmit from "@hooks/useSubmit";
 
-import { ChatInterface } from '@type/chat';
+import { ChatInterface } from "@type/chat";
 
-import PopupModal from '@components/PopupModal';
+import PopupModal from "@components/PopupModal";
+import { useTimeInputPlugin } from "../Plugin/TimeInput";
 
 const MessageContent = ({
   role,
@@ -35,8 +36,8 @@ const MessageContent = ({
   const [isEdit, setIsEdit] = useState<boolean>(sticky);
 
   return (
-    <div className='relative flex flex-col gap-1 md:gap-3 lg:w-[calc(100%-115px)]'>
-      <div className='flex flex-grow flex-col gap-3'></div>
+    <div className="relative flex flex-col gap-1 md:gap-3 lg:w-[calc(100%-115px)]">
+      <div className="flex flex-grow flex-col gap-3"></div>
       {isEdit ? (
         <EditView
           content={content}
@@ -86,13 +87,13 @@ const ContentView = React.memo(
       setChats(updatedChats);
     };
 
-    const handleMove = (direction: 'up' | 'down') => {
+    const handleMove = (direction: "up" | "down") => {
       const updatedChats: ChatInterface[] = JSON.parse(
         JSON.stringify(useStore.getState().chats)
       );
       const updatedMessages = updatedChats[currentChatIndex].messages;
       const temp = updatedMessages[messageIndex];
-      if (direction === 'up') {
+      if (direction === "up") {
         updatedMessages[messageIndex] = updatedMessages[messageIndex - 1];
         updatedMessages[messageIndex - 1] = temp;
       } else {
@@ -114,16 +115,16 @@ const ContentView = React.memo(
 
     return (
       <>
-        <div className='markdown prose w-full break-words dark:prose-invert dark'>
+        <div className="markdown prose w-full break-words dark:prose-invert dark">
           <ReactMarkdown
             remarkPlugins={[remarkMath, remarkGfm]}
-            rehypePlugins={[[rehypeKatex, { output: 'mathml' }]]}
+            rehypePlugins={[[rehypeKatex, { output: "mathml" }]]}
             components={{
               code({ node, inline, className, children, ...props }) {
                 if (inline) return <code>{children}</code>;
                 let highlight;
 
-                const match = /language-(\w+)/.exec(className || '');
+                const match = /language-(\w+)/.exec(className || "");
                 const lang = match && match[1];
                 if (lang)
                   highlight = hljs.highlight(children.toString(), {
@@ -132,11 +133,11 @@ const ContentView = React.memo(
                 else highlight = hljs.highlightAuto(children.toString());
 
                 return (
-                  <div className='bg-black rounded-md'>
-                    <div className='flex items-center relative text-gray-200 bg-gray-800 px-4 py-2 text-xs font-sans'>
-                      <span className=''>{highlight.language}</span>
+                  <div className="bg-black rounded-md">
+                    <div className="flex items-center relative text-gray-200 bg-gray-800 px-4 py-2 text-xs font-sans">
+                      <span className="">{highlight.language}</span>
                       <button
-                        className='flex ml-auto gap-2'
+                        className="flex ml-auto gap-2"
                         onClick={() => {
                           navigator.clipboard
                             .writeText(children.toString())
@@ -159,7 +160,7 @@ const ContentView = React.memo(
                         )}
                       </button>
                     </div>
-                    <div className='p-4 overflow-y-auto'>
+                    <div className="p-4 overflow-y-auto">
                       <code
                         className={`!whitespace-pre hljs language-${highlight.language}`}
                       >
@@ -176,24 +177,24 @@ const ContentView = React.memo(
                 );
               },
               p({ className, children, ...props }) {
-                return <p className='whitespace-pre-wrap'>{children}</p>;
+                return <p className="whitespace-pre-wrap">{children}</p>;
               },
             }}
           >
             {content}
           </ReactMarkdown>
         </div>
-        <div className='flex justify-end gap-2 w-full mt-2'>
+        <div className="flex justify-end gap-2 w-full mt-2">
           {isDelete || (
             <>
-              {role === 'assistant' && messageIndex === lastMessageIndex && (
+              {role === "assistant" && messageIndex === lastMessageIndex && (
                 <RefreshButton onClick={handleRefresh} />
               )}
               {messageIndex !== 0 && (
-                <UpButton onClick={() => handleMove('up')} />
+                <UpButton onClick={() => handleMove("up")} />
               )}
               {messageIndex !== lastMessageIndex && (
-                <DownButton onClick={() => handleMove('down')} />
+                <DownButton onClick={() => handleMove("down")} />
               )}
 
               <EditButton setIsEdit={setIsEdit} />
@@ -203,12 +204,12 @@ const ContentView = React.memo(
           {isDelete && (
             <>
               <button
-                className='p-1 hover:text-white'
+                className="p-1 hover:text-white"
                 onClick={() => setIsDelete(false)}
               >
                 <CrossIcon />
               </button>
-              <button className='p-1 hover:text-white' onClick={handleDelete}>
+              <button className="p-1 hover:text-white" onClick={handleDelete}>
                 <TickIcon />
               </button>
             </>
@@ -227,9 +228,9 @@ const MessageButton = ({
   icon: React.ReactElement;
 }) => {
   return (
-    <div className='text-gray-400 flex self-end lg:self-center justify-center gap-3 md:gap-4  visible'>
+    <div className="text-gray-400 flex self-end lg:self-center justify-center gap-3 md:gap-4  visible">
       <button
-        className='p-1 rounded-md hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200 disabled:dark:hover:text-gray-400 md:invisible md:group-hover:visible'
+        className="p-1 rounded-md hover:bg-gray-100 hover:text-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-gray-200 disabled:dark:hover:text-gray-400 md:invisible md:group-hover:visible"
         onClick={onClick}
       >
         {icon}
@@ -271,7 +272,7 @@ const UpButton = ({
 }) => {
   return (
     <MessageButton
-      icon={<DownChevronArrow className='rotate-180' />}
+      icon={<DownChevronArrow className="rotate-180" />}
       onClick={onClick}
     />
   );
@@ -300,23 +301,25 @@ const EditView = ({
   const setChats = useStore((state) => state.setChats);
   const currentChatIndex = useStore((state) => state.currentChatIndex);
 
+  const { TimeInput, formatTime } = useTimeInputPlugin();
   const [_content, _setContent] = useState<string>(content);
+  const getFullContent = () => formatTime() + _content;
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
   const textareaRef = React.createRef<HTMLTextAreaElement>();
 
   const resetTextAreaHeight = () => {
-    if (textareaRef.current) textareaRef.current.style.height = 'auto';
+    if (textareaRef.current) textareaRef.current.style.height = "auto";
   };
 
   const handleInput = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     if (textareaRef.current) {
-      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = "auto";
       textareaRef.current.style.height = `${e.target.scrollHeight}px`;
     }
   };
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if ((e.ctrlKey || e.shiftKey) && e.key === 'Enter') {
+    if ((e.ctrlKey || e.shiftKey) && e.key === "Enter") {
       e.preventDefault();
       if (sticky) {
         handleSaveAndSubmit();
@@ -326,17 +329,20 @@ const EditView = ({
   };
 
   const handleSave = () => {
-    if (sticky && _content === '') return;
+    if (sticky && _content === "") return;
     const updatedChats: ChatInterface[] = JSON.parse(
       JSON.stringify(useStore.getState().chats)
     );
     const updatedMessages = updatedChats[currentChatIndex].messages;
     if (sticky) {
-      updatedMessages.push({ role: inputRole, content: _content });
-      _setContent('');
+      updatedMessages.push({
+        role: inputRole,
+        content: getFullContent(),
+      });
+      _setContent("");
       resetTextAreaHeight();
     } else {
-      updatedMessages[messageIndex].content = _content;
+      updatedMessages[messageIndex].content = getFullContent();
       setIsEdit(false);
     }
     setChats(updatedChats);
@@ -349,13 +355,16 @@ const EditView = ({
     );
     const updatedMessages = updatedChats[currentChatIndex].messages;
     if (sticky) {
-      if (_content !== '') {
-        updatedMessages.push({ role: inputRole, content: _content });
+      if (_content !== "") {
+        updatedMessages.push({
+          role: inputRole,
+          content: getFullContent(),
+        });
       }
-      _setContent('');
+      _setContent("");
       resetTextAreaHeight();
     } else {
-      updatedMessages[messageIndex].content = _content;
+      updatedMessages[messageIndex].content = getFullContent();
       updatedChats[currentChatIndex].messages = updatedMessages.slice(
         0,
         messageIndex + 1
@@ -368,7 +377,7 @@ const EditView = ({
 
   useEffect(() => {
     if (textareaRef.current) {
-      textareaRef.current.style.height = 'auto';
+      textareaRef.current.style.height = "auto";
       textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
     }
   }, []);
@@ -376,15 +385,16 @@ const EditView = ({
   return (
     <>
       <div
-        className={`w-full ${
+        className={`w-full flex ${
           sticky
-            ? 'py-2 md:py-3 px-2 md:px-4 border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]'
-            : ''
+            ? "py-2 md:py-3 px-2 md:px-4 border border-black/10 bg-white dark:border-gray-900/50 dark:text-white dark:bg-gray-700 rounded-md shadow-[0_0_10px_rgba(0,0,0,0.10)] dark:shadow-[0_0_15px_rgba(0,0,0,0.10)]"
+            : ""
         }`}
       >
+        <TimeInput />
         <textarea
           ref={textareaRef}
-          className='m-0 resize-none rounded-lg bg-transparent overflow-y-hidden focus:ring-0 focus-visible:ring-0 leading-7 w-full'
+          className="m-0 resize-none rounded-lg bg-transparent overflow-y-hidden focus:ring-0 focus-visible:ring-0 leading-7 w-full"
           onChange={(e) => {
             _setContent(e.target.value);
           }}
@@ -394,13 +404,13 @@ const EditView = ({
           rows={1}
         ></textarea>
       </div>
-      <div className='text-center mt-2 flex justify-center'>
+      <div className="text-center mt-2 flex justify-center">
         {sticky && (
           <button
-            className='btn relative mr-2 btn-primary'
+            className="btn relative mr-2 btn-primary"
             onClick={handleSaveAndSubmit}
           >
-            <div className='flex items-center justify-center gap-2'>
+            <div className="flex items-center justify-center gap-2">
               Save & Submit
             </div>
           </button>
@@ -408,21 +418,21 @@ const EditView = ({
 
         <button
           className={`btn relative mr-2 ${
-            sticky ? 'btn-neutral' : 'btn-primary'
+            sticky ? "btn-neutral" : "btn-primary"
           }`}
           onClick={handleSave}
         >
-          <div className='flex items-center justify-center gap-2'>Save</div>
+          <div className="flex items-center justify-center gap-2">Save</div>
         </button>
 
         {sticky || (
           <button
-            className='btn relative mr-2 btn-neutral'
+            className="btn relative mr-2 btn-neutral"
             onClick={() => {
               setIsModalOpen(true);
             }}
           >
-            <div className='flex items-center justify-center gap-2'>
+            <div className="flex items-center justify-center gap-2">
               Save & Submit
             </div>
           </button>
@@ -430,18 +440,18 @@ const EditView = ({
 
         {sticky || (
           <button
-            className='btn relative btn-neutral'
+            className="btn relative btn-neutral"
             onClick={() => setIsEdit(false)}
           >
-            <div className='flex items-center justify-center gap-2'>Cancel</div>
+            <div className="flex items-center justify-center gap-2">Cancel</div>
           </button>
         )}
       </div>
       {isModalOpen && (
         <PopupModal
           setIsModalOpen={setIsModalOpen}
-          title='Warning'
-          message='Please be advised that by submitting this message, all subsequent messages will be deleted!'
+          title="Warning"
+          message="Please be advised that by submitting this message, all subsequent messages will be deleted!"
           handleConfirm={handleSaveAndSubmit}
         />
       )}
---
+++ src/components/Chat/ChatContent/Plugin/TimeInput.tsx
@@ -0,0 +1,128 @@
+import styled from "@emotion/styled";
+import React, {
+  ChangeEvent,
+  KeyboardEvent,
+  useCallback,
+  useState,
+} from "react";
+
+export const useTimeInputPlugin = (defaultTime?: Date) => {
+  const [time, setTime] = useState(defaultTime ?? new Date());
+  const DateTimeInput = useCallback(() => {
+    const hours = time.getHours();
+    const minutes = time.getMinutes();
+    const onUpdate = (updatedTime: { hours: number; minutes: number }) => {
+      const newTime = new Date(time);
+      newTime.setHours(updatedTime.hours);
+      newTime.setMinutes(updatedTime.minutes);
+      setTime(newTime);
+    };
+    return (
+      <TimeInput initMinutes={minutes} initHours={hours} onUpdate={onUpdate} />
+    );
+  }, []);
+  return { TimeInput: DateTimeInput, formatTime: () => formatTimeInDay(time) };
+};
+
+const TimeInput: React.FC<{
+  initHours?: number;
+  initMinutes?: number;
+  onUpdate: (time: { hours: number; minutes: number }) => void;
+}> = ({ initHours, initMinutes, onUpdate }) => {
+  const [hours, setHoursImpl] = useState(initHours ?? 0);
+  const [minutes, setMinutesImpl] = useState(initMinutes ?? 0);
+  const setHours = (hours: number) => {
+    setHoursImpl(hours);
+    onUpdate({ hours, minutes });
+  };
+  const setMinutes = (minutes: number) => {
+    setMinutesImpl(minutes);
+    onUpdate({ hours, minutes });
+  };
+
+  const handleHoursChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newHours = parseInt(event.target.value, 10);
+    if (isNaN(newHours) || newHours < 0 || newHours > 23) {
+      return;
+    }
+    setHours(newHours);
+  };
+
+  const handleMinutesChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newMinutes = parseInt(event.target.value, 10);
+    if (isNaN(newMinutes) || newMinutes < 0 || newMinutes > 59) {
+      return;
+    }
+    setMinutes(newMinutes);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    switch (event.key) {
+      case "ArrowUp": {
+        event.preventDefault();
+        if (event.currentTarget.name === "hours") {
+          const newHours = (hours + 1) % 24;
+          setHours(newHours);
+        } else {
+          const newMinutes = (minutes + 1) % 60;
+          setMinutes(newMinutes);
+        }
+        break;
+      }
+      case "ArrowDown": {
+        event.preventDefault();
+        if (event.currentTarget.name === "hours") {
+          const newHours = (hours + 23) % 24;
+          setHours(newHours);
+        } else {
+          const newMinutes = (minutes + 59) % 60;
+          setMinutes(newMinutes);
+        }
+        break;
+      }
+    }
+  };
+
+  return (
+    <div className="flex mx-1">
+      <ClearNumberInput
+        type="number"
+        name="hours"
+        min="0"
+        max="23"
+        value={hours}
+        onChange={handleHoursChange}
+        onKeyDown={handleKeyDown}
+      />
+      <span className="mx-1">:</span>
+      <ClearNumberInput
+        type="number"
+        name="minutes"
+        min="0"
+        max="59"
+        value={minutes}
+        onChange={handleMinutesChange}
+        onKeyDown={handleKeyDown}
+      />
+    </div>
+  );
+};
+
+const ClearNumberInput = styled.input`
+  background: transparent;
+  text-align: right;
+  & {
+    ::-webkit-outer-spin-button,
+    ::-webkit-inner-spin-button {
+      -webkit-appearance: none;
+      margin: 0;
+    }
+  }
+`;
+
+const formatTimeInDay = (time: Date) =>
+  Intl.DateTimeFormat("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: false,
+  }).format(time);
