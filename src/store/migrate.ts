9fa830d57d3019dfaa60b57a98730855d413dd8e
Jing Hua <tohjinghua123@gmail.com>
2023-03-12 13:55:21 +0800

    update migration

--- src/store/migrate.ts
+++ src/store/migrate.ts
@@ -1,5 +1,9 @@
 import { LocalStorageInterface } from '@type/chat';
+import { defaultChatConfig } from '@constants/chat';
 
 export const migrateV0 = (persistedState: LocalStorageInterface) => {
-  persistedState.chats.forEach((chat) => (chat.titleSet = false));
+  persistedState.chats.forEach((chat) => {
+    chat.titleSet = false;
+    if (!chat.config) chat.config = { ...defaultChatConfig };
+  });
 };
