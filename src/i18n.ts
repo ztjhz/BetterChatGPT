44dd951246eeaea932bd6d68df6bc500db6e0363
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-03-13 21:53:39 +0930

    Add zh-HK (Cantonese) translation

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,7 +4,7 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-export const i18nLanguages = ['en', 'zh-CN'];
+export const i18nLanguages = ['en', 'zh-CN', 'zh-HK'];
 
 i18n
   .use(Backend)
