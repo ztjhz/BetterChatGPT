7fcf69e5926e835f259f19d4134a8fc6b5fe55d9
flycran <cffhidol@163.com>
2023-03-16 08:17:38 +0800

    改用tailwindcss编写样式

--- src/components/Menu/CollapseOptions.tsx
+++ src/components/Menu/CollapseOptions.tsx
@@ -1,42 +1,16 @@
-import { css } from '@emotion/css';
 import ArrowBottom from '@icon/ArrowBottom';
 import React, { memo } from 'react';
 
-const style = css`
-  height: 12px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  & > div {
-  }
-
-  .border-b {
-    width: 100%;
-    flex: 1;
-  }
-
-  .x5 {
-    padding: 0 5px;
-    border-radius: 3px;
-    cursor: pointer;
-  }
-  
-  .fold {
-    transform: rotate(180deg);
-  }
-`;
-
 const CollapseOptions = memo((props: { onSetFold: Function; fold: boolean }) => {
-
   return (
-    <div className={ style }>
-      <div className='border-b border-white/20'></div>
-      <div className={ 'bg-gray-900 hover:bg-gray-500/10 transition-colors duration-200 x5 ' + (props.fold ? 'fold' : '') }
+    <div className='flex items-center'>
+      <div className='border-b border-white/20 flex-1'></div>
+      <div className='bg-gray-900 hover:bg-gray-500/10 transition-colors duration-200 px-3 rounded-sm cursor-pointer'
+           style={ { transform: props.fold ? 'rotate(180deg)' : '' } }
            onClick={ () => props.onSetFold(!props.fold) }>
         <ArrowBottom />
       </div>
-      <div className='border-b border-white/20'></div>
+      <div className='border-b border-white/20 flex-1'></div>
     </div>
   );
 });
