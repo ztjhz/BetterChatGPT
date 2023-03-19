e0155e3f4fe9b44984154f57b7386e368b5e556a
Jing Hua <tohjinghua123@gmail.com>
2023-03-19 13:33:53 +0800

    set sharegpt api endpoint as default

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,8 +1,9 @@
-export const defaultAPIEndpoint = 'https://chatgpt-api.shn.hk/v1/';
+export const defaultAPIEndpoint =
+  'https://sharegpt.churchless.tech/share/v1/chat';
 export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
 
 export const availableEndpoints = [
-  'https://chatgpt-api.shn.hk/v1/',
   'https://sharegpt.churchless.tech/share/v1/chat',
+  'https://chatgpt-api.shn.hk/v1/',
   officialAPIEndpoint,
 ];
