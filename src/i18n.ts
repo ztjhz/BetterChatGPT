1608991ac4d34274b407e0be0ea8b8c7e5ff0491
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-03-21 14:35:19 +0930

    add zh-TW translation

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,7 +4,7 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-export const i18nLanguages = ['en', 'da', 'zh-CN', 'zh-HK'];
+export const i18nLanguages = ['da', 'en', 'zh-CN', 'zh-HK', 'zh-TW'];
 
 i18n
   .use(Backend)
