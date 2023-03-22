a0995c6f8024ef8b15601d98c6d22e8af55053c2
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-03-22 16:15:11 +0930

    Fix nb-NO translation

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,14 +4,16 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-export const i18nLanguages = ['da', 'en', 'nb-NO', 'zh-CN', 'zh-HK', 'zh-TW'];
+export const i18nLanguages = ['da', 'en', 'nb', 'zh-CN', 'zh-HK', 'zh-TW'];
 
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    fallbackLng: 'en',
+    fallbackLng: {
+      default: ['en'],
+    },
     ns: ['main', 'api', 'about', 'model'],
     defaultNS: 'main',
   });
