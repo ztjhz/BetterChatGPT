395b3dad46106efb624e4f48c400f83de772f3be
MrSevyu <66242547+iiCodeRedemption@users.noreply.github.com>
2023-03-22 17:05:50 +0100

    Added Spanish translation. (#114)

--- src/i18n.ts
+++ src/i18n.ts
@@ -4,7 +4,16 @@ import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-export const i18nLanguages = ['da', 'en', 'nb', 'sv', 'zh-CN', 'zh-HK', 'zh-TW'];
+export const i18nLanguages = [
+  'da',
+  'en',
+  'es',
+  'nb',
+  'sv',
+  'zh-CN',
+  'zh-HK',
+  'zh-TW',
+];
 
 i18n
   .use(Backend)
