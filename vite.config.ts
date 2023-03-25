9574b4f750fbf7e1a13a084d931b4277e71bae43
ForCherry <105642081@qq.com>
2023-03-25 10:07:16 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/forcherry.github.io/FreeChatGPT/',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
