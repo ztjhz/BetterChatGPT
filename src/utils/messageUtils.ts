7a4afd265b58439bc9b374e747d4716f69af3dcd
Jing Hua <tohjinghua123@gmail.com>
2023-03-25 23:06:27 +0800

    fix: tiktoken performance

--- src/utils/messageUtils.ts
+++ src/utils/messageUtils.ts
@@ -3,23 +3,23 @@ import { MessageInterface, ModelOptions } from '@type/chat';
 import { Tiktoken } from '@dqbd/tiktoken/lite';
 const cl100k_base = await import('@dqbd/tiktoken/encoders/cl100k_base.json');
 
+const encoder = new Tiktoken(
+  cl100k_base.bpe_ranks,
+  {
+    ...cl100k_base.special_tokens,
+    '<|im_start|>': 100264,
+    '<|im_end|>': 100265,
+    '<|im_sep|>': 100266,
+  },
+  cl100k_base.pat_str
+);
+
 // https://github.com/dqbd/tiktoken/issues/23#issuecomment-1483317174
 export const getChatGPTEncoding = (
   messages: MessageInterface[],
   model: ModelOptions
 ) => {
   const isGpt3 = model === 'gpt-3.5-turbo';
-
-  const encoder = new Tiktoken(
-    cl100k_base.bpe_ranks,
-    {
-      ...cl100k_base.special_tokens,
-      '<|im_start|>': 100264,
-      '<|im_end|>': 100265,
-      '<|im_sep|>': 100266,
-    },
-    cl100k_base.pat_str
-  );
 
   const msgSep = isGpt3 ? '\n' : '';
   const roleSep = isGpt3 ? '\n' : '<|im_sep|>';
