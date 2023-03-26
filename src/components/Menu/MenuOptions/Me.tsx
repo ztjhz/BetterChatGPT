183070e7293e367f7e5e7f122c5872d431f1550d
pbk123461 <118950736+pbk123461@users.noreply.github.com>
2023-03-25 23:32:18 -0400

    Update Me.tsx

--- src/components/Menu/MenuOptions/Me.tsx
+++ src/components/Menu/MenuOptions/Me.tsx
@@ -8,7 +8,7 @@ const Me = () => {
   return (
     <a
       className='flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm'
-      href='https://github.com/pbk123/ChatGPT'
+      href='https://github.com/pbk123461/ChatGPT'
       target='_blank'
     >
       <HeartIcon />
