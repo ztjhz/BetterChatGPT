2632742c131e498e89cdcca421c0a165d390f8f0
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-03-27 15:15:39 +1000

    Fix translation bug

--- src/i18n.ts
+++ src/i18n.ts
@@ -5,7 +5,7 @@ import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 export const i18nLanguages = [
-  'ar',
+  // 'ar',
   'da',
   'en',
   'es',
