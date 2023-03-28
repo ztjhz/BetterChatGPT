3fbdb6aa5a45d46d78a88675ed14d5eb056e6a94
pbk123461 <118950736+pbk123461@users.noreply.github.com>
2023-03-28 13:44:25 -0400

    Update Me.tsx

--- src/components/Menu/MenuOptions/Me.tsx
+++ src/components/Menu/MenuOptions/Me.tsx
@@ -8,11 +8,11 @@ const Me = () => {
   return (
     <a
       className='flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white cursor-pointer text-sm'
-      href='https://github.com/ztjhz/BetterChatGPT'
+      href='https://github.com/pbk123461/ChatGPT'
       target='_blank'
     >
       <HeartIcon />
-      {t('author')}
+      {t('Made by Jing Hua. This website is made by pbk123461')}
     </a>
   );
 };
