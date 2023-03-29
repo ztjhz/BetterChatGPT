25576212a3a8580b18a433489b1fcec913c4879b
Tindo N. Arsel <devtnga@gmail.com>
2023-03-29 12:46:36 +0100

    Instruct the system to respond  using Markdown (#161)
    This for a default behavior.
    This makes the answer of the AI more user-friendly to read.
    This also increases the accuracy of the answer to be well-formatted.

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -11,6 +11,8 @@ const dateString =
 
 // default system message obtained using the following method: https://twitter.com/DeminDimin/status/1619935545144279040
 export const _defaultSystemMessage = `You are ChatGPT, a large language model trained by OpenAI.
+Carefully heed the user's instructions. 
+Respond using Markdown.
 Knowledge cutoff: 2021-09
 Current date: ${dateString}`;
 
