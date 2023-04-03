a73cb590aa56d081cee7115a2a317bbdf14918fd
Jing Hua <tohjinghua123@gmail.com>
2023-04-04 00:27:09 +0800

    style: folder gradient

--- src/components/Menu/ChatFolder.tsx
+++ src/components/Menu/ChatFolder.tsx
@@ -36,6 +36,7 @@ const ChatFolder = ({
 
   const inputRef = useRef<HTMLInputElement>(null);
   const folderRef = useRef<HTMLDivElement>(null);
+  const gradientRef = useRef<HTMLDivElement>(null);
 
   const [_folderName, _setFolderName] = useState<string>(folderName);
   const [isEdit, setIsEdit] = useState<boolean>(false);
@@ -154,16 +155,18 @@ const ChatFolder = ({
         style={{ background: color || '' }}
         className={`${
           color ? '' : 'hover:bg-gray-850'
-        } transition-colors flex py-3 px-3 items-center gap-3 relative rounded-md break-all cursor-pointer`}
+        } transition-colors flex py-3 pl-3 pr-1 items-center gap-3 relative rounded-md break-all cursor-pointer`}
         onClick={toggleExpanded}
         ref={folderRef}
         onMouseEnter={() => {
           if (color && folderRef.current)
             folderRef.current.style.background = `${color}dd`;
+          if (gradientRef.current) gradientRef.current.style.width = '0px';
         }}
         onMouseLeave={() => {
           if (color && folderRef.current)
             folderRef.current.style.background = color;
+          if (gradientRef.current) gradientRef.current.style.width = '1rem';
         }}
       >
         <FolderIcon className='h-4 w-4' />
@@ -182,6 +185,19 @@ const ChatFolder = ({
             />
           ) : (
             _folderName
+          )}
+          {isEdit || (
+            <div
+              ref={gradientRef}
+              className='absolute inset-y-0 right-0 w-4 z-10 transition-all'
+              style={{
+                background:
+                  color &&
+                  `linear-gradient(to left, ${
+                    color || 'var(--color-900)'
+                  }, rgb(32 33 35 / 0))`,
+              }}
+            />
           )}
         </div>
         <div
@@ -257,7 +273,7 @@ const ChatFolder = ({
         </div>
       </div>
       <div className='group/folder ml-3 pl-1 border-l-2 border-gray-700 flex flex-col gap-1'>
-        {isExpanded && <NewChat compact folder={folderId} />}
+        {isExpanded && <NewChat folder={folderId} />}
         {isExpanded &&
           folderChats.map((chat) => (
             <ChatHistory
