ffb1a9b0726dc692baafa0d1c7d826e61aef390e
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-04-04 12:20:43 +0800

    Add language names

--- src/components/LanguageSelector/LanguageSelector.tsx
+++ src/components/LanguageSelector/LanguageSelector.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import DownChevronArrow from '@icon/DownChevronArrow';
-import { i18nLanguages } from '@src/i18n';
+import { languageCodeToName, selectableLanguages } from '@constants/language';
 
 const LanguageSelector = () => {
   const { i18n } = useTranslation();
@@ -11,24 +11,25 @@ const LanguageSelector = () => {
   return (
     <div className='prose dark:prose-invert relative'>
       <button
-        className='btn btn-neutral btn-small flex w-32 flex justify-between'
+        className='btn btn-neutral btn-small w-36 flex justify-between'
         type='button'
         onClick={() => setDropDown((prev) => !prev)}
       >
-        {i18n.language}
+        {languageCodeToName[i18n.language as keyof typeof languageCodeToName] ??
+          i18n.language}
         <DownChevronArrow />
       </button>
       <div
         id='dropdown'
         className={`${
           dropDown ? '' : 'hidden'
-        } absolute top-100 bottom-100 z-10 bg-white rounded-lg shadow-xl border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group dark:bg-gray-800 opacity-90 w-32`}
+        } absolute top-100 bottom-100 z-10 bg-white rounded-lg shadow-xl border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group dark:bg-gray-800 opacity-90 w-36`}
       >
         <ul
-          className='text-sm text-gray-700 dark:text-gray-200 p-0 m-0'
+          className='text-sm text-gray-700 dark:text-gray-200 p-0 m-0 max-h-72 overflow-auto'
           aria-labelledby='dropdownDefaultButton'
         >
-          {i18nLanguages.map((lang) => (
+          {selectableLanguages.map((lang) => (
             <li
               className='px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer'
               onClick={() => {
@@ -37,7 +38,7 @@ const LanguageSelector = () => {
               }}
               key={lang}
             >
-              {lang}
+              {languageCodeToName[lang]}
             </li>
           ))}
         </ul>
