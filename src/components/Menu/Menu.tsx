7e7df206ae9ab73635476561b780d1a712413969
Jing Hua <tohjinghua123@gmail.com>
2023-04-06 02:54:22 +0800

    style: padding
    fixes #203

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -37,7 +37,7 @@ const Menu = () => {
       >
         <div className='flex h-full min-h-0 flex-col'>
           <div className='flex h-full w-full flex-1 items-start border-white/20'>
-            <nav className='flex h-full flex-1 flex-col space-y-1 p-2'>
+            <nav className='flex h-full flex-1 flex-col space-y-1 px-2 pt-2'>
               <NewChat />
               <ChatHistoryList />
               <MenuOptions />
