723b190b4a4bc8efcaeb5674ccf0f385ba97dca0
Jing Hua <tohjinghua123@gmail.com>
2023-04-07 21:03:36 +0800

    dynamic import katex.css and add yarn.lock

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'katex/dist/katex.css'
 import App from './App';
 import './main.css';
+await import('katex/dist/katex.min.css');
 
 import './i18n';
 
