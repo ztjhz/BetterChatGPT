a2123cd222996f1dc03e5b500b4d327ade78b227
tnga <devtnga@gmail.com>
2023-04-11 08:33:45 +0100

    Refactor: menu NewFolder styling classnames
    Changed the hidden class to mb-2,
    so that the plus icon on new folder button will appear in mobile view.

--- src/components/Menu/NewFolder.tsx
+++ src/components/Menu/NewFolder.tsx
@@ -43,7 +43,7 @@ const NewFolder = () => {
 
   return (
     <a
-      className={`max-md:hidden flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white text-sm md:mb-2 flex-shrink-0 md:border md:border-white/20 transition-opacity ${
+      className={`flex py-3 px-3 items-center gap-3 rounded-md hover:bg-gray-500/10 transition-colors duration-200 text-white text-sm mb-2 flex-shrink-0 border border-white/20 transition-opacity ${
         generating
           ? 'cursor-not-allowed opacity-40'
           : 'cursor-pointer opacity-100'
