8d99d329444bfb43bde7d93ca2ff19e02b418c7f
Jing Hua <tohjinghua123@gmail.com>
2023-04-20 13:39:10 +0800

    remove console log

--- src/hooks/useHideOnOutsideClick.ts
+++ src/hooks/useHideOnOutsideClick.ts
@@ -14,7 +14,6 @@ const useHideOnOutsideClick = (): [
       !elementRef.current.contains(event.target as Node)
     ) {
       setShowElement(false);
-      console.log(Math.random());
     }
   };
 
