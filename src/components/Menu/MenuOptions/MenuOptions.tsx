d4947134c2f15643ea256afbc16474319699318f
Jack Schedel <jackschedel@gmail.com>
2023-05-08 13:50:12 -0400

    moved clear conversation into settings, revamped to match style

--- src/components/Menu/MenuOptions/MenuOptions.tsx
+++ src/components/Menu/MenuOptions/MenuOptions.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import useStore from '@store/store';
 
-import ClearConversation from './ClearConversation';
 import Api from './Api';
 import Me from './Me';
 import AboutMenu from '@components/AboutMenu';
@@ -27,7 +26,6 @@ const MenuOptions = () => {
         {countTotalTokens && <TotalTokenCostDisplay />}
         {googleClientId && <GoogleSync clientId={googleClientId} />}
         <AboutMenu />
-        <ClearConversation />
         <ImportExportChat />
         <Api />
         <SettingsMenu />
