8f6140f0237b3742c11713fffe647229a75ebc68
Jianfeng Liu <jfliu@sequoiacap.com>
2023-05-27 23:44:06 +0800

    new chat

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -5,6 +5,7 @@ import ChatContent from './ChatContent';
 import MobileBar from '../MobileBar';
 import StopGeneratingButton from '@components/StopGeneratingButton/StopGeneratingButton';
 import HorizontalMenu from '@components/HorizontalMenu/menu';
+import NewChat from '@components/Menu/NewChat';
 
 const Chat = () => {
   const hideSideMenu = useStore((state) => state.hideSideMenu);
@@ -14,6 +15,9 @@ const Chat = () => {
       className={`flex h-full flex-1 flex-col ${hideSideMenu ? 'md:pl-0' : 'md:pl-[260px]'
         }`}
     >
+      <div className='absolute right-2 top-2 z-50'>
+        <NewChat />
+      </div>
       <main className='relative bg-gray-50 h-full w-full transition-width flex flex-col overflow-hidden items-stretch flex-1'>
         <HorizontalMenu />
         <ChatContent />
