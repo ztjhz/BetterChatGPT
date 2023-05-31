3ab42609c4d2b06028304fd2097602ba5db498a1
Ayaka Mikazuki <ayaka@mail.shn.hk>
2023-05-31 23:40:24 +0800

    Fix formatting

--- src/i18n.ts
+++ src/i18n.ts
@@ -7,43 +7,43 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID || undefined;
 
 export const i18nLanguages = [
-	// 'ar',
-	'da',
-	'en',
-	'en-GB',
-	'en-US',
-	'es',
-	'fr',
-	'fr-FR',
-	'it',
-	'ja',
-	'ms',
-	'nb',
-	'sv',
-	// 'ug',
-	'yue',
-	'zh-CN',
-	'zh-HK',
-	'zh-TW',
-	'ro',
+  // 'ar',
+  'da',
+  'en',
+  'en-GB',
+  'en-US',
+  'es',
+  'fr',
+  'fr-FR',
+  'it',
+  'ja',
+  'ms',
+  'nb',
+  'sv',
+  // 'ug',
+  'yue',
+  'zh-CN',
+  'zh-HK',
+  'zh-TW',
+  'ro',
 ];
 
 const namespace = ['main', 'api', 'about', 'model'];
 if (googleClientId) namespace.push('drive');
 
 i18n
-	.use(Backend)
-	.use(LanguageDetector)
-	.use(initReactI18next)
-	.init({
-		backend: {
-			loadPath: 'locales/{{lng}}/{{ns}}.json',
-		},
-		fallbackLng: {
-			default: ['en'],
-		},
-		ns: namespace,
-		defaultNS: 'main',
-	});
+  .use(Backend)
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    backend: {
+      loadPath: 'locales/{{lng}}/{{ns}}.json',
+    },
+    fallbackLng: {
+      default: ['en'],
+    },
+    ns: namespace,
+    defaultNS: 'main',
+  });
 
 export default i18n;
