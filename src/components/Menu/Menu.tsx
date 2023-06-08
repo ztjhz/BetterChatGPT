bbcd76a35988b80844ca8fd23a0d51ab3c499d4f
Zabir Raihan <zabirraihan6@gmal.com>
2023-06-08 21:21:51 +0800

    Draggable & inline titlebar on MacOS electron app

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -39,7 +39,7 @@ const Menu = () => {
         <div className='flex h-full min-h-0 flex-col'>
           <div className='flex h-full w-full flex-1 items-start border-white/20'>
             <nav className='flex h-full flex-1 flex-col space-y-1 px-2 pt-2'>
-              <div className='flex gap-2'>
+              <div className='drag justify-end flex gap-2'>
                 <NewChat />
                 <NewFolder />
               </div>
--- src/components/Menu/NewChat.tsx
+++ src/components/Menu/NewChat.tsx
@@ -10,15 +10,19 @@ const NewChat = ({ folder }: { folder?: string }) => {
   const { t } = useTranslation();
   const addChat = useAddChat();
   const generating = useStore((state) => state.generating);
+  const isElectron = navigator.userAgent.indexOf('Electron') !== -1;
+  const isMacOS = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
 
   return (
     <a
-      className={`flex flex-1 items-center rounded-md hover:bg-gray-500/10 transition-all duration-200 text-white text-sm flex-shrink-0 ${
+      className={`no-drag flex items-center rounded-md hover:bg-gray-500/10 transition-all duration-200 text-white text-sm flex-shrink-0 ${
         generating
           ? 'cursor-not-allowed opacity-40'
           : 'cursor-pointer opacity-100'
       } ${
         folder ? 'justify-start' : 'py-2 px-2 gap-3 mb-2 border border-white/20'
+      } ${
+        isElectron && isMacOS ? '' : 'flex-1'
       }`}
       onClick={() => {
         if (!generating) addChat(folder);
