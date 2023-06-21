50d4df608b8219956dc31454b2a78b6ee50fef5b
7flash <igorberlenko7@gmail.com>
2023-06-21 14:59:33 +0000

    resolveWebLinks /a2106

---
+++ replit.nix
@@ -0,0 +1,10 @@
+{ pkgs }: {
+    deps = [
+        pkgs.yarn
+        pkgs.esbuild
+        pkgs.nodejs-16_x
+
+        pkgs.nodePackages.typescript
+        pkgs.nodePackages.typescript-language-server
+    ];
+}
