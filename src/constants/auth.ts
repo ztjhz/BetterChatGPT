8125f87d947f605344d20fa0796d2334c61756cb
Vishal Vyas <imvishalvyas@gmail.com>
2023-06-27 12:28:40 +0530

    Update oathkeeper proxy endpoint

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,4 +1,4 @@
-export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
+export const officialAPIEndpoint = 'https://dev-proxy.zurutech.online/public/openai-public';
 const customAPIEndpoint =
   import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://chatgpt-api.shn.hk/v1/';
 export const defaultAPIEndpoint =
