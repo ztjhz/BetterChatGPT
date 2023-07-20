2a459dcb33dd56f21a78800b7bf487e7d957a383
Andrew O'Reilly <andrworlly@gmail.com>
2023-07-20 17:41:47 -0400

    Add "advanced model" dropdown expander to ModelSelector

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -22,9 +22,9 @@ export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo-16k',
   'gpt-4',
   'gpt-4-32k',
-  // 'gpt-3.5-turbo-0301',
-  // 'gpt-4-0314',
-  // 'gpt-4-32k-0314',
+  'gpt-3.5-turbo-0301',
+  'gpt-4-0314',
+  'gpt-4-32k-0314',
 ];
 
 export const defaultModel = 'gpt-3.5-turbo';
