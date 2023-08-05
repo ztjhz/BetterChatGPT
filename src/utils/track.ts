11a74ebf034fe1c7fcfbbdac62882562e2fc352a
docacola <liu@qna3.ai>
2023-08-05 17:38:31 +0800

    sentry sample rate

--- src/utils/track.ts
+++ src/utils/track.ts
@@ -13,7 +13,7 @@ export const trackingToolsInit = () => {
       new Sentry.Replay(),
     ],
     // Performance Monitoring
-    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!
+    tracesSampleRate:  process.env.VITE_SENTRY_ENV === 'development' ? 1.0 : 0.6, // Capture 100% of the transactions, reduce in production!
     // Session Replay
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
