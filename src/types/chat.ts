0399dec25e1067eeeae48ef3540d96bf6385e2d9
RedwindA <128586631+RedwindA@users.noreply.github.com>
2023-08-11 02:34:10 +0800

    add models

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -49,10 +49,7 @@ export interface Folder {
   color?: string;
 }
 
-export type ModelOptions = 'gpt-4' | 'gpt-4-32k' | 'gpt-3.5-turbo' | 'gpt-3.5-turbo-16k' ;
-// | 'gpt-3.5-turbo-0301';
-// | 'gpt-4-0314'
-// | 'gpt-4-32k-0314'
+export type ModelOptions = 'gpt-4' | 'gpt-4-32k' | 'gpt-3.5-turbo' | 'gpt-3.5-turbo-16k' | 'gpt-3.5-turbo-0301'|'gpt-3.5-turbo-0613'| 'gpt-4-0314'| 'gpt-4-32k-0314'
 
 export type TotalTokenUsed = {
   [model in ModelOptions]?: {
