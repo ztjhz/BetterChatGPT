5624171c05153592b5a8c180eb9cffb80bb97b7a
Tanuj <Tanuj@cmu.edu>
2023-09-12 18:56:10 -0700

    Fix scrollToBottom incompatibility

--- src/components/Chat/ChatContent/ScrollToBottomButton.tsx
+++ src/components/Chat/ChatContent/ScrollToBottomButton.tsx
@@ -7,13 +7,17 @@ const ScrollToBottomButton = React.memo(() => {
   const scrollToBottom = useScrollToBottom();
   const [atBottom] = useAtBottom();
 
+  const handleClick: React.MouseEventHandler<HTMLButtonElement> = () => {
+    scrollToBottom( {behavior: 'smooth'} );
+  };
+
   return (
     <button
       className={`cursor-pointer absolute right-6 bottom-[60px] md:bottom-[60px] z-10 rounded-full border border-gray-200 bg-gray-50 text-gray-600 dark:border-white/10 dark:bg-white/10 dark:text-gray-200 ${
         atBottom ? 'hidden' : ''
       }`}
       aria-label='scroll to bottom'
-      onClick={scrollToBottom}
+      onClick={handleClick}
     >
       <DownArrow />
     </button>
