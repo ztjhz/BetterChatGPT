28c0b880e28866887aa2e2114114ddc493160e9d
maozdemir <ozdemirmus16@itu.edu.tr>
2023-11-21 16:15:16 +0300

    feat: Add gpt-3.5-turbo-1106, update prices. (#482)
    * feat: Add gpt-3.5-turbo-1106, update prices.
    
    * Update package.json
    
    ---------
    
    Co-authored-by: Jing Hua <tohjinghua123@gmail.com>

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -49,7 +49,7 @@ export interface Folder {
   color?: string;
 }
 
-export type ModelOptions = 'gpt-4' | 'gpt-4-32k' | 'gpt-3.5-turbo' | 'gpt-3.5-turbo-16k' | 'gpt-4-1106-preview' ;
+export type ModelOptions = 'gpt-4' | 'gpt-4-32k' | 'gpt-4-1106-preview' | 'gpt-3.5-turbo' | 'gpt-3.5-turbo-16k' | 'gpt-3.5-turbo-1106' ;
 // | 'gpt-3.5-turbo-0301';
 // | 'gpt-4-0314'
 // | 'gpt-4-32k-0314'
