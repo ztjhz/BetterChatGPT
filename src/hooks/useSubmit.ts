83f2c6eac81ae9d5384052229f4bda88322a9fcb
Slendy_Milky <slendymilky@gmail.com>
2023-12-26 13:04:34 +0100

    Update useSubmit.ts

--- src/hooks/useSubmit.ts
+++ src/hooks/useSubmit.ts
@@ -100,33 +100,34 @@ const useSubmit = () => {
         );
       }
 
-      let messageQueue = [];
-      let streamProcessingCompleted = false;
-      async function processMessageQueue() {
-        while (!streamProcessingCompleted || messageQueue.length > 0) {
-          if (messageQueue.length > 0) {
-            let messageContent = messageQueue.shift();
-            const messageLen = messageContent.length;
-            let curChars = '';
-            let speed = streamProcessingCompleted
-              ? messageLen
-              : Math.max(3, Math.round(messageLen / 30));
-            while (messageContent) {
-              curChars = messageContent.slice(0, speed);
-              const updatedChats = JSON.parse(
-                JSON.stringify(useStore.getState().chats)
-              );
-              const updatedMessages = updatedChats[currentChatIndex].messages;
-              updatedMessages[updatedMessages.length - 1].content += curChars;
-              setChats(updatedChats);
-              messageContent = messageContent.slice(speed);
-              await new Promise((resolve) => setTimeout(resolve, 50));
-            }
-          } else {
+    let messageQueue: string[] = [];
+    let streamProcessingCompleted = false;
+
+    async function processMessageQueue() {
+      while (!streamProcessingCompleted || messageQueue.length > 0) {
+        if (messageQueue.length > 0) {
+          let messageContent = messageQueue.shift();
+          const messageLen = messageContent.length;
+          let curChars = '';
+          let speed = streamProcessingCompleted
+            ? messageLen
+            : Math.max(3, Math.round(messageLen / 30));
+          while (messageContent) {
+            curChars = messageContent.slice(0, speed);
+            const updatedChats = JSON.parse(
+              JSON.stringify(useStore.getState().chats)
+            );
+            const updatedMessages = updatedChats[currentChatIndex].messages;
+            updatedMessages[updatedMessages.length - 1].content += curChars;
+            setChats(updatedChats);
+            messageContent = messageContent.slice(speed);
             await new Promise((resolve) => setTimeout(resolve, 50));
           }
+        } else {
+          await new Promise((resolve) => setTimeout(resolve, 50));
         }
       }
+    }
       async function readStream(stream) {
         if (stream.locked)
           throw new Error(
