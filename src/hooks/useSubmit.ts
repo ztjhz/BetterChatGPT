e9240b420b60c84a1cdaadd7dbc2eb84fdc3ad9a
Slendy_Milky <slendymilky@gmail.com>
2023-12-26 14:41:39 +0100

    Update useSubmit.ts

--- src/hooks/useSubmit.ts
+++ src/hooks/useSubmit.ts
@@ -7,7 +7,7 @@ import { parseEventSource } from '@api/helper';
 import { limitMessageTokens, updateTotalTokenUsed } from '@utils/messageUtils';
 import { _defaultChatConfig } from '@constants/chat';
 import { officialAPIEndpoint } from '@constants/auth';
-import { ReadableStream } from 'web-streams-polyfill/ponyfill/es2018';
+import { ReadableStream } from 'web-streams-polyfill/ponyfill';
 
 
 const useSubmit = () => {
