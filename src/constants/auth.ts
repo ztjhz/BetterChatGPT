bcc0e8ef21306445ffe27f7b1b8f269f5071696a
Slendy_Milky <slendymilky@gmail.com>
2024-01-17 00:12:21 +0100

    Revert "changement deuxième url"
    This reverts commit 329daed11ea576f9c28320da461f6c147f0a4d3f.

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,6 +1,6 @@
 export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
 const customAPIEndpoint =
-  import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://api.mistral.ai/v1/chat/completions';
+  import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://chatgpt-api.shn.hk/v1/';
 export const defaultAPIEndpoint =
   import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
 
