b1f0f1904fb5a86586213ec186f64d749229c220
Slendy_Milky <slendymilky@gmail.com>
2024-01-16 23:59:32 +0100

    Update chat.ts

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -22,9 +22,7 @@ export interface ConfigInterface {
   model: ModelOptions;
   max_tokens: number;
   temperature: number;
-  presence_penalty: number;
   top_p: number;
-  frequency_penalty: number;
 }
 
 export interface ChatHistoryInterface {
