49bc559455036010242419fffdecce6fb207949d
Slendy_Milky <slendymilky@gmail.com>
2024-01-17 00:14:18 +0100

    Revert "Update ChatConfigMenu.tsx"
    This reverts commit d8c1b3561841ae434ae31453945f4c941345b489.

--- src/components/ChatConfigMenu/ChatConfigMenu.tsx
+++ src/components/ChatConfigMenu/ChatConfigMenu.tsx
@@ -4,8 +4,10 @@ import { useTranslation } from 'react-i18next';
 
 import PopupModal from '@components/PopupModal';
 import {
+  FrequencyPenaltySlider,
   MaxTokenSlider,
   ModelSelector,
+  PresencePenaltySlider,
   TemperatureSlider,
   TopPSlider,
 } from '@components/ConfigMenu/ConfigMenu';
@@ -48,6 +50,12 @@ const ChatConfigPopup = ({
   const [_maxToken, _setMaxToken] = useState<number>(config.max_tokens);
   const [_temperature, _setTemperature] = useState<number>(config.temperature);
   const [_topP, _setTopP] = useState<number>(config.top_p);
+  const [_presencePenalty, _setPresencePenalty] = useState<number>(
+    config.presence_penalty
+  );
+  const [_frequencyPenalty, _setFrequencyPenalty] = useState<number>(
+    config.frequency_penalty
+  );
 
   const { t } = useTranslation('model');
 
@@ -69,6 +77,8 @@ const ChatConfigPopup = ({
     _setMaxToken(_defaultChatConfig.max_tokens);
     _setTemperature(_defaultChatConfig.temperature);
     _setTopP(_defaultChatConfig.top_p);
+    _setPresencePenalty(_defaultChatConfig.presence_penalty);
+    _setFrequencyPenalty(_defaultChatConfig.frequency_penalty);
     _setSystemMessage(_defaultSystemMessage);
   };
 
@@ -94,6 +104,14 @@ const ChatConfigPopup = ({
           _setTemperature={_setTemperature}
         />
         <TopPSlider _topP={_topP} _setTopP={_setTopP} />
+        <PresencePenaltySlider
+          _presencePenalty={_presencePenalty}
+          _setPresencePenalty={_setPresencePenalty}
+        />
+        <FrequencyPenaltySlider
+          _frequencyPenalty={_frequencyPenalty}
+          _setFrequencyPenalty={_setFrequencyPenalty}
+        />
         <div
           className='btn btn-neutral cursor-pointer mt-5'
           onClick={handleReset}
