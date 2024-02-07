12a67b48390e2c03b19504720a25eacccfe30b47
Florentin Sakwiset <florentin@wemake.cx>
2024-02-07 22:16:21 +0100

    Remove gpt-3.5-turbo-0301 model option

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -25,7 +25,7 @@ export const modelOptions: ModelOptions[] = [
   'gpt-4-32k',
   'gpt-4-1106-preview',
   'gpt-4-0125-preview',
-  'gpt-3.5-turbo-0301',
+  //  'gpt-3.5-turbo-0301',
   'gpt-4-0314',
   'gpt-4-32k-0314',
 ];
