8adce3ca4d2eb25bd78708fccce2e9b17d21595a
lerejbox <jerelongbx@gmail.com>
2024-02-15 23:11:12 +0800

    minor changes: change config menu button dropdown color

--- src/components/ConfigMenu/ConfigMenu.tsx
+++ src/components/ConfigMenu/ConfigMenu.tsx
@@ -95,7 +95,7 @@ export const ModelSelector = ({
         id='dropdown'
         className={`${
           dropDown ? '' : 'hidden'
-        } absolute top-100 bottom-100 z-10 bg-white rounded-lg shadow-xl border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group dark:bg-gray-800 opacity-90`}
+        } absolute top-100 bottom-100 z-10 bg-white rounded-lg shadow-xl border-b border-black/10 dark:border-gray-900/50 text-gray-800 dark:text-gray-100 group dark:new-chat-light`}
       >
         <ul
           className='text-sm text-gray-700 dark:text-gray-200 p-0 m-0'
@@ -103,7 +103,7 @@ export const ModelSelector = ({
         >
           {modelOptions.map((m) => (
             <li
-              className='px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer'
+              className='px-4 py-2 hover:bg-gray-100 dark:hover:new-chat-light-hover dark:hover:text-white cursor-pointer'
               onClick={() => {
                 _setModel(m);
                 setDropDown(false);
