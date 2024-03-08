437f18ac3ad33a9481ddbf4ca73a099482577442
Florentin Sakwiset <florentin@wemake.cx>
2024-03-08 02:20:01 +0100

    Update chat constants and types

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -1,44 +1,45 @@
-import { v4 as uuidv4 } from "uuid";
-import { ChatInterface, ConfigInterface, ModelOptions } from "@type/chat";
-import useStore from "@store/store";
+import { v4 as uuidv4 } from 'uuid';
+import { ChatInterface, ConfigInterface, ModelOptions } from '@type/chat';
+import useStore from '@store/store';
 
 const date = new Date();
 const dateString =
   date.getFullYear() +
-  "-" +
-  ("0" + (date.getMonth() + 1)).slice(-2) +
-  "-" +
-  ("0" + date.getDate()).slice(-2);
+  '-' +
+  ('0' + (date.getMonth() + 1)).slice(-2) +
+  '-' +
+  ('0' + date.getDate()).slice(-2);
 
 // default system message obtained using the following method: https://twitter.com/DeminDimin/status/1619935545144279040
 export const _defaultSystemMessage =
   import.meta.env.VITE_DEFAULT_SYSTEM_MESSAGE ??
-  `Hey, 👋
-  WeMake great stuff, and you're part of it. 💙`;
+  `You are ChatGPT, a large language model trained by OpenAI.
+Carefully heed the user's instructions. 
+Respond using Markdown.`;
 
 export const modelOptions: ModelOptions[] = [
   'gpt-3.5-turbo',
-  //  'gpt-3.5-turbo-16k',
-  //  'gpt-3.5-turbo-1106',
+  'gpt-3.5-turbo-16k',
+  'gpt-3.5-turbo-1106',
   'gpt-3.5-turbo-0125',
   'gpt-4',
   'gpt-4-32k',
   'gpt-4-1106-preview',
-  'gpt-4-0125-preview',
-  'gpt-3.5-turbo-0301',
-  'gpt-4-0314',
-  'gpt-4-32k-0314',
+  'gpt-4-0125-preview'
+  // 'gpt-3.5-turbo-0301',
+  // 'gpt-4-0314',
+  // 'gpt-4-32k-0314',
 ];
 
-export const defaultModel = "gpt-3.5-turbo";
+export const defaultModel = 'gpt-3.5-turbo';
 
 export const modelMaxToken = {
   'gpt-3.5-turbo': 4096,
   'gpt-3.5-turbo-0301': 4096,
-  //  'gpt-3.5-turbo-0613': 4096,
-  //  'gpt-3.5-turbo-16k': 16384,
-  //  'gpt-3.5-turbo-16k-0613': 16384,
-  //  'gpt-3.5-turbo-1106': 16384,
+  'gpt-3.5-turbo-0613': 4096,
+  'gpt-3.5-turbo-16k': 16384,
+  'gpt-3.5-turbo-16k-0613': 16384,
+  'gpt-3.5-turbo-1106': 16384,
   'gpt-3.5-turbo-0125': 16384,
   'gpt-4': 8192,
   'gpt-4-0314': 8192,
@@ -51,30 +52,30 @@ export const modelMaxToken = {
 };
 
 export const modelCost = {
-  "gpt-3.5-turbo": {
+  'gpt-3.5-turbo': {
     prompt: { price: 0.0015, unit: 1000 },
     completion: { price: 0.002, unit: 1000 },
   },
-   "gpt-3.5-turbo-0301": {
-      prompt: { price: 0.0015, unit: 1000 },
-      completion: { price: 0.002, unit: 1000 },
-    },
-  /*   "gpt-3.5-turbo-0613": {
-      prompt: { price: 0.0015, unit: 1000 },
-      completion: { price: 0.002, unit: 1000 },
-    }, */
-  /*   "gpt-3.5-turbo-16k": {
-      prompt: { price: 0.003, unit: 1000 },
-      completion: { price: 0.004, unit: 1000 },
-    }, */
-  /*   "gpt-3.5-turbo-16k-0613": {
-      prompt: { price: 0.003, unit: 1000 },
-      completion: { price: 0.004, unit: 1000 },
-    }, */
-  /*   "gpt-3.5-turbo-1106": {
-      prompt: { price: 0.001, unit: 1000 },
-      completion: { price: 0.0015, unit: 1000 },
-    }, */
+  'gpt-3.5-turbo-0301': {
+    prompt: { price: 0.0015, unit: 1000 },
+    completion: { price: 0.002, unit: 1000 },
+  },
+  'gpt-3.5-turbo-0613': {
+    prompt: { price: 0.0015, unit: 1000 },
+    completion: { price: 0.002, unit: 1000 },
+  },
+  'gpt-3.5-turbo-16k': {
+    prompt: { price: 0.003, unit: 1000 },
+    completion: { price: 0.004, unit: 1000 },
+  },
+  'gpt-3.5-turbo-16k-0613': {
+    prompt: { price: 0.003, unit: 1000 },
+    completion: { price: 0.004, unit: 1000 },
+  },
+  'gpt-3.5-turbo-1106': {
+    prompt: { price: 0.001, unit: 1000 },
+    completion: { price: 0.0015, unit: 1000 },
+  },
   'gpt-3.5-turbo-0125': {
     prompt: { price: 0.0005, unit: 1000 },
     completion: { price: 0.0015, unit: 1000 },
@@ -83,31 +84,31 @@ export const modelCost = {
     prompt: { price: 0.03, unit: 1000 },
     completion: { price: 0.06, unit: 1000 },
   },
-  "gpt-4-0314": {
+  'gpt-4-0314': {
     prompt: { price: 0.03, unit: 1000 },
     completion: { price: 0.06, unit: 1000 },
   },
-  "gpt-4-0613": {
+  'gpt-4-0613': {
     prompt: { price: 0.03, unit: 1000 },
     completion: { price: 0.06, unit: 1000 },
   },
-  "gpt-4-32k": {
+  'gpt-4-32k': {
     prompt: { price: 0.06, unit: 1000 },
     completion: { price: 0.12, unit: 1000 },
   },
-  "gpt-4-32k-0314": {
+  'gpt-4-32k-0314': {
     prompt: { price: 0.06, unit: 1000 },
     completion: { price: 0.12, unit: 1000 },
   },
-  "gpt-4-32k-0613": {
+  'gpt-4-32k-0613': {
     prompt: { price: 0.06, unit: 1000 },
     completion: { price: 0.12, unit: 1000 },
   },
-  "gpt-4-1106-preview": {
+  'gpt-4-1106-preview': {
     prompt: { price: 0.01, unit: 1000 },
     completion: { price: 0.03, unit: 1000 },
   },
-  "gpt-4-0125-preview": {
+  'gpt-4-0125-preview': {
     prompt: { price: 0.01, unit: 1000 },
     completion: { price: 0.03, unit: 1000 },
   },
@@ -126,13 +127,13 @@ export const _defaultChatConfig: ConfigInterface = {
 
 export const generateDefaultChat = (
   title?: string,
-  folder?: string,
+  folder?: string
 ): ChatInterface => ({
   id: uuidv4(),
-  title: title ? title : "New Chat",
+  title: title ? title : 'New Chat',
   messages:
     useStore.getState().defaultSystemMessage.length > 0
-      ? [{ role: "system", content: useStore.getState().defaultSystemMessage }]
+      ? [{ role: 'system', content: useStore.getState().defaultSystemMessage }]
       : [],
   config: { ...useStore.getState().defaultChatConfig },
   titleSet: false,
@@ -140,39 +141,39 @@ export const generateDefaultChat = (
 });
 
 export const codeLanguageSubset = [
-  "python",
-  "javascript",
-  "java",
-  "go",
-  "bash",
-  "c",
-  "cpp",
-  "csharp",
-  "css",
-  "diff",
-  "graphql",
-  "json",
-  "kotlin",
-  "less",
-  "lua",
-  "makefile",
-  "markdown",
-  "objectivec",
-  "perl",
-  "php",
-  "php-template",
-  "plaintext",
-  "python-repl",
-  "r",
-  "ruby",
-  "rust",
-  "scss",
-  "shell",
-  "sql",
-  "swift",
-  "typescript",
-  "vbnet",
-  "wasm",
-  "xml",
-  "yaml",
+  'python',
+  'javascript',
+  'java',
+  'go',
+  'bash',
+  'c',
+  'cpp',
+  'csharp',
+  'css',
+  'diff',
+  'graphql',
+  'json',
+  'kotlin',
+  'less',
+  'lua',
+  'makefile',
+  'markdown',
+  'objectivec',
+  'perl',
+  'php',
+  'php-template',
+  'plaintext',
+  'python-repl',
+  'r',
+  'ruby',
+  'rust',
+  'scss',
+  'shell',
+  'sql',
+  'swift',
+  'typescript',
+  'vbnet',
+  'wasm',
+  'xml',
+  'yaml',
 ];
--- src/types/chat.ts
+++ src/types/chat.ts
@@ -1,8 +1,8 @@
-import { Prompt } from "./prompt";
-import { Theme } from "./theme";
+import { Prompt } from './prompt';
+import { Theme } from './theme';
 
-export type Role = "user" | "assistant" | "system";
-export const roles: Role[] = ["user", "assistant", "system"];
+export type Role = 'user' | 'assistant' | 'system';
+export const roles: Role[] = ['user', 'assistant', 'system'];
 
 export interface MessageInterface {
   role: Role;
@@ -55,12 +55,12 @@ export type ModelOptions =
   | 'gpt-4-1106-preview'
   | 'gpt-4-0125-preview'
   | 'gpt-3.5-turbo'
-  //  | 'gpt-3.5-turbo-16k'
-  //  | 'gpt-3.5-turbo-1106'
-  | 'gpt-3.5-turbo-0125'
-  | 'gpt-3.5-turbo-0301'
-  | 'gpt-4-0314'
-  | 'gpt-4-32k-0314'
+  | 'gpt-3.5-turbo-16k'
+  | 'gpt-3.5-turbo-1106'
+  | 'gpt-3.5-turbo-0125';
+// | 'gpt-3.5-turbo-0301';
+// | 'gpt-4-0314'
+// | 'gpt-4-32k-0314'
 
 export type TotalTokenUsed = {
   [model in ModelOptions]?: {
