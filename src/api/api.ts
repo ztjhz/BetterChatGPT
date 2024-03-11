d716a195f8748e9b24d8a65413c535b10e16c31f
DmitriyAlergant-T1A <dmitriy.alergant@t1a.com>
2024-03-11 00:48:23 -0400

    Complete models cleanup

--- src/api/api.ts
+++ src/api/api.ts
@@ -18,22 +18,7 @@ export const getChatCompletion = async (
   if (isAzureEndpoint(endpoint) && apiKey) {
     headers['api-key'] = apiKey;
 
-    const modelmapping: Partial<Record<ModelOptions, string>> = {
-      'gpt-3.5-turbo': 'gpt-35-turbo',
-      'gpt-3.5-turbo-16k': 'gpt-35-turbo-16k',
-      'gpt-3.5-turbo-1106': 'gpt-35-turbo-1106',
-      'gpt-3.5-turbo-0125': 'gpt-35-turbo-0125',
-    };
-
-    const model = modelmapping[config.model] || config.model;
-
-    // set api version to 2023-07-01-preview for gpt-4 and gpt-4-32k, otherwise use 2023-03-15-preview
-    const apiVersion =
-      model === 'gpt-4' || model === 'gpt-4-32k'
-        ? '2023-07-01-preview'
-        : '2023-03-15-preview';
-
-    const path = `openai/deployments/${model}/chat/completions?api-version=${apiVersion}`;
+    const path = `chat/completions`;
 
     if (!endpoint.endsWith(path)) {
       if (!endpoint.endsWith('/')) {
@@ -74,19 +59,7 @@ export const getChatCompletionStream = async (
   if (isAzureEndpoint(endpoint) && apiKey) {
     headers['api-key'] = apiKey;
 
-    const modelmapping: Partial<Record<ModelOptions, string>> = {
-      'gpt-3.5-turbo': 'gpt-35-turbo',
-      'gpt-3.5-turbo-16k': 'gpt-35-turbo-16k',
-    };
-
-    const model = modelmapping[config.model] || config.model;
-
-    // set api version to 2023-07-01-preview for gpt-4 and gpt-4-32k, otherwise use 2023-03-15-preview
-    const apiVersion =
-      model === 'gpt-4' || model === 'gpt-4-32k'
-        ? '2023-07-01-preview'
-        : '2023-03-15-preview';
-    const path = `openai/deployments/${model}/chat/completions?api-version=${apiVersion}`;
+    const path = `chat/completions`;
 
     if (!endpoint.endsWith(path)) {
       if (!endpoint.endsWith('/')) {
--- src/types/chat.ts
+++ src/types/chat.ts
@@ -50,18 +50,9 @@ export interface Folder {
 }
 
 export type ModelOptions =
+  'gpt-3.5-turbo'
   | 'gpt-4'
-  | 'gpt-4-32k'
-  | 'gpt-4-1106-preview'
-  | 'gpt-4-0125-preview'
-  | 'gpt-3.5-turbo'
-  | 'gpt-3.5-turbo-16k'
-  | 'gpt-3.5-turbo-1106'
-  | 'gpt-3.5-turbo-0125'
-  | 'gpt-4-turbo-preview'
-  | 'gpt-3.5-turbo-0301'
-  | 'gpt-4-0314'
-  | 'gpt-4-32k-0314';
+  | 'gpt-4-turbo-preview';
 
 export type TotalTokenUsed = {
   [model in ModelOptions]?: {
