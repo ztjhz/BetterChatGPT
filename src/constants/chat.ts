99dd2a23e710f2041c1548fddbfd27cb2ba4a7df
DmitriyAlergant-T1A <dmitriy.alergant@t1a.com>
2024-03-11 12:13:36 -0400

    Improve verbiage for model settings

--- src/constants/chat.ts
+++ src/constants/chat.ts
@@ -49,7 +49,7 @@ export const modelCost = {
   }
 };
 
-export const defaultUserMaxToken = 8000;
+export const defaultUserMaxToken = 4000;
 
 export const _defaultChatConfig: ConfigInterface = {
   model: defaultModel,
