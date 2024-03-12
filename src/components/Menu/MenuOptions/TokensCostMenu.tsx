c51eb7962819049501a5136520888964a0f6fb14
DmitriyAlergant-T1A <dmitriy.alergant@t1a.com>
2024-03-12 10:02:16 -0400

    Costs Modal - fix font for dark mode

--- src/components/Menu/MenuOptions/TokensCostMenu.tsx
+++ src/components/Menu/MenuOptions/TokensCostMenu.tsx
@@ -23,25 +23,27 @@ const TokensCostMenu = () => {
           cancelButton={false}
         >
           <>
-            <div className='p-3'>
-              The AI model costs are tracked for your awareness, and to build healthy usage habits.<br/>
-              The metrics are counted locally, and are per device/browser. You can reset at any time.
-            </div>  
-            <div className='p-3'>
-              Long conversation threads are the primary driver of AI costs creep. With each message in the thread, the app is submitting the <u>entire thread history</u> (from the beginning) to the LLM model to evaluate. We recommend starting a new chat frequently to lighten the load.
+            <div className="text-gray-700 dark:text-gray-300">
+              <div className='p-3'>
+                The AI model costs are tracked for your awareness, and to build healthy usage habits.<br/>
+                The metrics are counted locally, and are per device/browser. You can reset at any time.
+              </div>  
+              <div className='p-3'>
+                Long conversation threads are the primary driver of AI costs creep. With each message in the thread, the app is submitting the <u>entire thread history</u> (from the beginning) to the LLM model to evaluate. We recommend starting a new chat frequently to lighten the load.
+              </div>
+              <div className='p-3'>
+                Consider smaller model (GPT 3.5) when questions are easy, you may not need full power.
+              </div>
+              <div className='p-3'>
+                <b>The AI LLMs are helping us working effeciently; Use it as much as needed!</b>
+              </div>  
+              <div className='p-3'>
+                <TotalTokenCost />
+              </div>
+              <div className='p-3'>
+                If you use it so much that your monthly costs consistently exceed $30 USD per month,<br/>talk to your manager regarding alternative "unmetered" premium access options.
+              </div>  
             </div>
-            <div className='p-3'>
-              Consider smaller model (GPT 3.5) when questions are easy, you may not need full power.
-            </div>
-            <div className='p-3'>
-              <b>The AI LLMs are helping us working effeciently; Use it as much as needed!</b>
-            </div>  
-            <div className='p-3'>
-              <TotalTokenCost />
-            </div>
-            <div className='p-3'>
-              If you use it so much that your monthly costs consistently exceed $30 USD per month,<br/>talk to your manager regarding alternative "unmetered" premium access options.
-            </div>  
           </>
         </PopupModal>
       )}
