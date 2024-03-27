a716205c73279f6f07bebf274d87e7003bb6fbab
Dmitriy Alergant <dmitriy.alergant@t1a.com>
2024-03-28 00:15:20 +0100

    Fix disappearing header (fix to a fx)

--- src/components/Chat/ChatContent/ChatHeader.tsx
+++ src/components/Chat/ChatContent/ChatHeader.tsx
@@ -45,7 +45,7 @@ const ChatHeader = React.memo(() => {
   return config ? (
     <>
       <div
-        className='absolute gap-x-4 gap-y-1 flex-wrap w-full items-center justify-center border-b border-black/10 bg-gray-50 p-3 dark:border-gray-900/50 dark:bg-gray-700 text-gray-600 dark:text-gray-300 cursor-pointer'
+        className='absolute flex top-0 left-0 right-0 gap-x-4 gap-y-1 flex-wrap w-full items-center justify-center border-b border-black/10 bg-gray-50 p-3 dark:border-gray-900/50 dark:bg-gray-700 text-gray-600 dark:text-gray-300 cursor-pointer'
         onClick={() => {
           setIsModalOpen(true);
         }}
