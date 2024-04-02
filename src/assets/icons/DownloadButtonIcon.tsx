89a8703444ad55723faa9d6eeda7a8953070a3a1
Dmitriy Alergant <dmitriy.alergant@t1a.com>
2024-04-02 23:29:33 +0200

    Make download button larger

--- src/assets/icons/DownloadButtonIcon.tsx
+++ src/assets/icons/DownloadButtonIcon.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DownloadButtonIcon = (props: React.SVGProps<SVGSVGElement>) => {
     return (
-<svg width="16px" height="16px" viewBox="0 0 24 24" fill="none">
+<svg width="22px" height="22px" viewBox="0 0 24 24" fill="none">
 <path d="M5.25589 16C3.8899 15.0291 3 13.4422 3 11.6493C3 9.20008 4.8 6.9375 7.5 6.5C8.34694 4.48637 10.3514 3 12.6893 3C15.684 3 18.1317 5.32251 18.3 8.25C19.8893 8.94488 21 10.6503 21 12.4969C21 14.0582 20.206 15.4339 19 16.2417M12 21V11M12 21L9 18M12 21L15 18" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
 </svg>
   );
