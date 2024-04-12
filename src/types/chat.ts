42ff19ca323c52e6e60631f532ebc9915307510d
ChenYuZe519 <chenyuewan@icloud.com>
2024-04-12 23:04:38 +0800

    Added support for "gpt-4-turbo-2024-04-09" and "gpt-4-turbo" models. (#572)
    * Update chat.ts
    
    Added support for "gpt-4-turbo-2024-04-09" and "gpt-4-turbo" models.
    
    * Update chat.ts
    
    Added support for "gpt-4-turbo-2024-04-09" and "gpt-4-turbo" models.

--- src/types/chat.ts
+++ src/types/chat.ts
@@ -54,6 +54,8 @@ export type ModelOptions =
   | 'gpt-4-32k'
   | 'gpt-4-1106-preview'
   | 'gpt-4-0125-preview'
+  | 'gpt-4-turbo'
+  | 'gpt-4-turbo-2024-04-09'
   | 'gpt-3.5-turbo'
   | 'gpt-3.5-turbo-16k'
   | 'gpt-3.5-turbo-1106'
